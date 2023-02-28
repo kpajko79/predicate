9d2bb42047d6e2317191a4c5e20bce3074286755
Patrik Kluba <kpajko79@gmail.com>
2023-02-28 07:00:42 +0100

    Extract integer_sequence into a separate header

--- predicate.hpp
+++ predicate.hpp
@@ -34,6 +34,8 @@
 #include <vector>
 #include <cmath>
 
+#include "integer_sequence.hpp"
+
 namespace pajko {
 
 namespace Predicate {
@@ -65,32 +67,6 @@ protected:
 namespace impl {
 
 static std::vector<std::unique_ptr<Predicate>> holder;
-
-namespace detail {
-
-template<typename T, T... Ints>
-struct integer_sequence
-{
-  typedef T value_type;
-  static constexpr std::size_t size() { return sizeof...(Ints); }
-};
-
-template<std::size_t... Ints>
-using index_sequence = integer_sequence<std::size_t, Ints...>;
-
-template<typename T, std::size_t N, T... Is>
-struct make_integer_sequence : make_integer_sequence<T, N-1, N-1, Is...> {};
-
-template<typename T, T... Is>
-struct make_integer_sequence<T, 0, Is...> : integer_sequence<T, Is...> {};
-
-template<std::size_t N>
-using make_index_sequence = make_integer_sequence<std::size_t, N>;
-
-template<typename... T>
-using index_sequence_for = make_index_sequence<sizeof...(T)>;
-
-}; /* namespace detail */
 
 template <typename T>
 class EncapsulatorImpl : public Encapsulator
